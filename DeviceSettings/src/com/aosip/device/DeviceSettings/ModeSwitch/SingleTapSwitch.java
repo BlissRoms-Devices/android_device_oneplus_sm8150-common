e6730784abd5c791b5c85154d44595ae6bcd5011
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-02-23 21:19:13 -0700

    sm8150-common: sepolicy: Address camera denials

--- DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/SingleTapSwitch.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/SingleTapSwitch.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package org.lineageos.device.DeviceSettings;
+package com.aosip.device.DeviceSettings;
 
 import android.content.Context;
 import android.content.SharedPreferences;
