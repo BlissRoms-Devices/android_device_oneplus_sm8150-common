e6eb8d74d2fada278ee91cee1c0043973bfae74b
Lup Gabriel <gwolf2u.website@gmail.com>
2020-02-27 12:53:51 +0200

    sm8150-common: squash revert FOD commits
    Revert "sm8150-common: fod: Correct dim amount for HBM mode"
    This reverts commit bc5c6f3385efd158a6ba861802b6bc772fa9531d.
    
    Revert "sm8150-common: fod: Use new FOD dimming alpha node"
    This reverts commit e3e15d9b9c2ec1cc6451ade2078c9e93a69071cb.
    
    Revert "sm8150-common: fod: Disable DC-Dimming when fod circle is visible"
    This reverts commit 511480951dc067757fecbf828464633848990825.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -35,8 +35,7 @@
 
 // This is not a typo by me. It's by OnePlus.
 #define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
-#define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/fod_dim_alpha"
-#define HBM_PATH "/sys/class/drm/card0-DSI-1/hbm"
+#define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
 
 namespace vendor {
 namespace lineage {
@@ -157,10 +156,6 @@ Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
 
 Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
     int dimAmount = get(DIM_AMOUNT_PATH, 0);
-    int hbmMode = get(HBM_PATH, 0);
-    if (hbmMode == 5) {
-        dimAmount = 42;
-    }
     LOG(INFO) << "dimAmount = " << dimAmount;
 
     return dimAmount;
