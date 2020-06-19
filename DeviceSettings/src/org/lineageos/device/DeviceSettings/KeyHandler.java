1c20d0a35d97d5b7a45f845aa344fef0ad9f9b5d
Lup Gabriel <gwolf2u.website@gmail.com>
2020-06-19 10:32:42 +0300

    sm8150-common: set client package name to lineage

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/KeyHandler.java
@@ -84,7 +84,7 @@ public class KeyHandler implements DeviceKeyHandler {
     }
 
     public static final String CLIENT_PACKAGE_NAME = "com.oneplus.camera";
-    public static final String CLIENT_PACKAGE_PATH = "/data/misc/crdroid/client_package_name";
+    public static final String CLIENT_PACKAGE_PATH = "/data/misc/lineage/client_package_name";
 
     // Single tap key code
     private static final int SINGLE_TAP = 67;
