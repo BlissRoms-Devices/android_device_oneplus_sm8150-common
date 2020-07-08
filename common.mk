e9589ccfccb124229288f2b6c3fee7c64273a473
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-07-07 22:22:18 -0500

    sdm845-common: Introduce DeviceSettings
    - Imported from https://github.com/AOSiP-Devices/device_oneplus_sdm845-common/tree/f55c0218951d9b0f6586bdf5041a64ea14c96460/DeviceSettings
    - Adapted to AndroidX
    - Includes all translations from pie cycle
    - Dropped DC-Dimming QS Tile
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -449,10 +449,13 @@ PRODUCT_PACKAGES += \
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oneplus_msmnile
+    
+# AOSiP Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
