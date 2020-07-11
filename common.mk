eae9e92181e727fe95f9d99da69904b193a8f292
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-07-11 18:43:38 -0500

    sm8150-common: Add initial OnePlusCamera support

--- common.mk
+++ common.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Inherit packages from vendor/oneplus/camera
+$(call inherit-product, vendor/oneplus/camera/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
