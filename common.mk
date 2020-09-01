cf43ccc645264885e3436bf924e11565c81b7aa8
Jackeagle <jackeagle102@gmail.com>
2020-08-31 20:45:08 -0500

    sm8150-common: Use CustomDoze

--- common.mk
+++ common.mk
@@ -243,6 +243,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.0.vendor
 
+# Doze
+PRODUCT_PACKAGES += \
+    CustomDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
