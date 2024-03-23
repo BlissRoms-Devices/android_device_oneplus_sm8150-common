6f13a125e2a50832a14079ff76d77c6f2c99f0b9
srgrusso <srgrusso240@gmail.com>
2024-03-23 13:53:05 -0500

    Revert "sm8150-common: Keep building health HIDL interface lib in vendor"
    This reverts commit 631b69053c6f4073019a51883e6b3721244ab494.

--- common.mk
+++ common.mk
@@ -204,9 +204,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor
-
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
