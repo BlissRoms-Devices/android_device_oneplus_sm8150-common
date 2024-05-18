43dbeb674db510d90aa76dde89e5ba6577a3a743
Michael Bestas <mkbestas@lineageos.org>
2024-05-18 09:14:16 +0300

    sm8150-common: Move to common power-libperfmgr
    Change-Id: Ia5d5cd84d557d95ee447a321bc81e3c8fa2c2c11

--- common.mk
+++ common.mk
@@ -340,11 +340,10 @@ PRODUCT_BOOT_JARS += \
     oplus-fwk
 
 # Power
-$(call inherit-product, hardware/oplus/libqti-perfd-client/libqti-perfd-client.mk)
-$(call inherit-product, hardware/oplus/power-libperfmgr/power-libperfmgr.mk)
-
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2.vendor
+    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power@1.2.vendor \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -398,7 +397,11 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/oplus \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
