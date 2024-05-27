e0f40f1a9c30c7545302af7beaffd7a50800ed1a
Dhina17 <dhinalogu@gmail.com>
2024-05-27 19:19:11 +0530

    sm8150-common: Impl torch light control
    Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    
    [RealJohnGalt:] modify for oneplus usage, add toggle switch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,9 @@ USE_CUSTOM_AUDIO_POLICY := 1
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true
 
+# Camera
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.opsm8150
+
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
