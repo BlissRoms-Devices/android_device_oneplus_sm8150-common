fac70b303e677c5fd10440e479004dd3cc4152c6
ShevT <evgeny.shishkov@gmail.com>
2024-08-23 12:11:44 +0300

    sm8150-common: Move oplus-fwk to hardware_oplus repo

--- common.mk
+++ common.mk
@@ -351,13 +351,6 @@ PRODUCT_PACKAGES += \
     OPlusExtrasResCommon \
     SimpleDeviceConfigCommon
 
-# Oplus-fwk
-PRODUCT_PACKAGES += \
-    oplus-fwk
-
-PRODUCT_BOOT_JARS += \
-    oplus-fwk
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
@@ -452,6 +445,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+
+$(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
 # Touch
 PRODUCT_PACKAGES += \
