6cd0c848a4c2616a0b6c0b1d103411d8e048bb0e
LuK1337 <priv.luk@gmail.com>
2024-08-28 15:32:59 +0200

    sm8150-common: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- common.mk
+++ common.mk
@@ -452,6 +452,8 @@ $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oplus
 
+$(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
