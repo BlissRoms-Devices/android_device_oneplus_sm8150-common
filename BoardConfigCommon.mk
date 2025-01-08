f7d911127922430fc3480ad9c5d00cbf2974530f
ShevT <evgeny.shishkov@gmail.com>
2025-01-08 12:02:48 +0300

    sm8150-common: Add hardware usb aidl to device framework matrix
    Change-Id: Idec232b0dd90fd6c831f4ee7e026cde604073ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,6 +59,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
