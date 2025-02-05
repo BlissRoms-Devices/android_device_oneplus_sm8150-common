c6171330818b029f873a3076813749627b42b52a
ShevT <evgeny.shishkov@gmail.com>
2025-02-05 09:32:31 +0300

    sm8150-common: Move media configs to configs folder
    Change-Id: I2ae16bad109d5b271bae00ade6ad132bdd584bf0

--- common.mk
+++ common.mk
@@ -213,8 +213,8 @@ $(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_DM,true)
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
-    $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
 
 # NFC
 PRODUCT_PACKAGES += \
