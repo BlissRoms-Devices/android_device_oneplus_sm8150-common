219d3597dd5d323eb5e69dc077741384fbfabfae
srgrusso <srgrusso240@gmail.com>
2025-04-19 05:50:09 -0500

    Init Bliss

--- common.mk
+++ common.mk
@@ -232,6 +232,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-bliss \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
